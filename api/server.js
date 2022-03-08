6f8c7c48c8b242e4f17e9568b129753ed251de24
XavierTwitty <x.twitty@gmail.com>
2022-03-08 14:22:57 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -2,9 +2,18 @@
 // require your posts router and connect it here
 const express =  require('express')
 
+const postRouter = require('./posts/posts-router')
+
 const server = express()
 
 server.use(express.json())
 
+server.use('/api/posts', postRouter )
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h2> welcome to api </h2>
+    `)
+})
 
 module.exports = server
